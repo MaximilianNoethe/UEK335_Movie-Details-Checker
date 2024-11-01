d328ba42b53ac8fd33eb6767f3f2dd1b9e4784d1
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-11-01 11:01:15 +0100

    changed styling

--- movie-checker/App/component/molecule/NavBar.tsx
+++ movie-checker/App/component/molecule/NavBar.tsx
@@ -21,7 +21,7 @@ export default function NavBar() {
                         right: 0,
                         left: 0,
                         elevation: 0,
-                        height: 60,
+                        height: 90,
                         backgroundColor: "#621827",
                         paddingBottom: 6
                     },
@@ -33,10 +33,10 @@ export default function NavBar() {
                     component={MoviePage}
                     options={{
                         tabBarIcon : ({focused})=> (
-                            <IconButton icon="movie" size={30} iconColor={focused ?"#B0B0B0": "#FFFFFF"}/>
+                            <IconButton icon="movie" size={40} iconColor={focused ?"#B0B0B0": "#FFFFFF"} style={{marginBottom: -5}}/>
                         ),
                         tabBarLabel : ({focused})=>(
-                            <Text style={{ color : focused ? "#B0B0B0": "#FFFFFF", fontSize: 12 }}>
+                            <Text style={{ color : focused ? "#B0B0B0": "#FFFFFF", fontSize: 15 , marginBottom: 5}}>
                                 Movies
                             </Text>
                         ),
@@ -47,10 +47,10 @@ export default function NavBar() {
                     component={HomePage}
                     options={{
                         tabBarIcon : ({focused})=> (
-                            <IconButton icon="home" size={30} iconColor={focused ?"#B0B0B0": "#FFFFFF"}/>
+                            <IconButton icon="home" size={40} iconColor={focused ?"#B0B0B0": "#FFFFFF"} style={{marginBottom: -5}}/>
                         ),
                         tabBarLabel : ({focused})=>(
-                            <Text style={{ color : focused ? "#B0B0B0": "#FFFFFF", fontSize: 12 }}>
+                            <Text style={{ color : focused ? "#B0B0B0": "#FFFFFF", fontSize: 12 , marginBottom: 5}}>
                                 Home
                             </Text>
                         ),
@@ -61,10 +61,10 @@ export default function NavBar() {
                     component={UserProfilePage}
                     options={{
                         tabBarIcon : ({focused})=> (
-                            <IconButton icon="account-circle" size={30} iconColor={focused ?"#B0B0B0": "#FFFFFF"}/>
+                            <IconButton icon="account-circle" size={40} iconColor={focused ?"#B0B0B0": "#FFFFFF"} style={{marginBottom: -5}}/>
                         ),
                         tabBarLabel : ({focused})=>(
-                            <Text style={{ color : focused ? "#B0B0B0": "#FFFFFF", fontSize: 12 }}>
+                            <Text style={{ color : focused ? "#B0B0B0": "#FFFFFF", fontSize: 12 , marginBottom: 5}}>
                                 Profile
                             </Text>
                         ),
