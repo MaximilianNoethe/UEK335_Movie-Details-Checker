2eaa8982c4b3d34ca8ac8b4132df67d01c8ff7a6
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-11-01 16:16:43 +0100

    finished MovieCard Template

--- movie-checker/App/component/page/MoviePage.tsx
+++ movie-checker/App/component/page/MoviePage.tsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MovieCard from "../molecule/MovieCard";
+import {MovieDetails} from "../../models/models";
+
+const testmovie : MovieDetails = {
+    Title: "Mad Max",
+    "Release Date": "Okt 18 2006",
+    "MPAA Rating": "R",
+    "Running Time min": "120min",
+    "Major Genre": "Horror",
+    Director: "Martin Scorcese",
+    "Rotten Tomatoes Rating": 20,
+    "IMDB Rating" : 7.3,
+    id: 2
+}
 
 
 export default function MoviePage() {
     return (
         <View style={styles.container}>
-            <Text>MoviePage</Text>
+            <MovieCard movie={testmovie} />
         </View>
-    );
+);
 }
 
 const styles = StyleSheet.create({
