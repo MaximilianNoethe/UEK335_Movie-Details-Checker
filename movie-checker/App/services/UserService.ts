29f6ec4e3f32b020028c8955609a07464f99245e
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-11-02 16:39:12 +0100

    implement getUserData function - getCurrentUserData function still needs to be adjusted

--- movie-checker/App/services/UserService.ts
+++ movie-checker/App/services/UserService.ts
@@ -1,5 +1,7 @@
 import { AxiosInstance } from 'axios';
-import { defaultInstance } from '../api/Api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { api } from "../api/Api";
+import { getAuthorizationToken, login } from './AuthService';
 
 export type User = {
     userId: string;
@@ -7,22 +9,53 @@ export type User = {
     firstname: string;
     lastname: string;
     age: number;
-}
-// Typ von getCurrentUserData so anpassen, dass nur userId übergeben wird
-const UserService = (api: AxiosInstance = defaultInstance) => ({
-    getCurrentUserData: async (userId: string) => { // nur userId als Argument
+};
+
+const email = "gianluca@noseryoung.ch";
+const password = "bestPassw0rd";
+
+const config = getAuthorizationToken();
+
+const UserService = () => ({
+    getUserData: async () => {
+        await login(email, password);
         try {
+            const userId = await AsyncStorage.getItem("userId");
+            const response = await api.get(`users/${userId}`, config);
+            return response.data;
+    } catch (error) {
+        console.error("Error occurred:", error);
+        return {} as User;
+    }
+    },
+
+
+    getCurrentUserData: async (): Promise<User> => {
+        try {
+            const userId = await AsyncStorage.getItem("userId");
+            if (!userId) {
+                console.log("User ID not found in AsyncStorage");
+                return {} as User;
+            }
+
             const response = await api.get(`users/${userId}`);
             return response.data;
         } catch (error) {
             console.log("Error occurred:", error);
+            return {} as User;
         }
     },
 
     updateUser: async (user: User) => {
         try {
+            const userId = await AsyncStorage.getItem("userId");
+            if (!userId) {
+                console.log("User ID not found in AsyncStorage");
+                return;
+            }
+
             const data = { user };
-            const response = await api.put(`users/${user.userId}`, data);
+            const response = await api.put(`users/${userId}`, data);
             return response.data;
         } catch (error) {
             console.error("Error occurred:", error);
