92a47f1e97e2967dcf812ef67fbf8852eae14863
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-11-02 16:38:21 +0100

    Implement new version of login function - still needs to be adjusted

--- movie-checker/App/services/AuthService.ts
+++ movie-checker/App/services/AuthService.ts
@@ -1,25 +1,70 @@
 import { AxiosInstance } from "axios";
-import { defaultInstance } from "../api/Api";
+/*import { defaultInstance } from "../api/Api";*/
+import { api } from "../api/Api";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
-export type loginRequest = {
+
+
+
+export const login = async (email: string, password: string) => {
+    try {
+      const response = await api.post("login", {
+        email: email,
+        password: password,
+      });
+      const { accessToken, user } = response.data;
+      AsyncStorage.setItem("accessToken", accessToken);
+      await AsyncStorage.setItem("userId", user.id.toString());
+
+    return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+
+  export const getAuthorizationToken = () => {
+    const accessToken = AsyncStorage.getItem("accessToken");
+    if (!accessToken) {
+      throw new Error("no token");
+    }
+    return {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+  };
+
+
+
+
+
+
+
+
+/*export type loginRequest = {
     email: "gianluca@noseryoung.ch", //string
-    password: "bestPassword0" //string
+    password: "bestPassw0rd" //string
 }
 
 const LoginService = (api: AxiosInstance = defaultInstance) => ({
     login: async (param: loginRequest) => {
       const answer = await api.post("login", param);
       const { accessToken, user } = answer.data;
-      console.log(answer.data.accessToken);
-
-      AsyncStorage.setItem("accessToken", accessToken);
-      AsyncStorage.setItem("userId", user.id.toString());
+  
+      await AsyncStorage.setItem("accessToken", accessToken);
+      if (user && user.id) {
+        await AsyncStorage.setItem("userId", user.id.toString());
+        console.log("User ID saved to AsyncStorage:", user.id);
+      } else {
+        console.log("User ID is missing in the response data");
+      }
   
       return answer;
     }
   });
 
-  export default LoginService;
+*/
+  export default login;
   
