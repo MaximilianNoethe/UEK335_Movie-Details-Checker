32033240d42677b223672885154977efd264fccc
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-11-02 16:34:35 +0100

    add UserProfilePage to App.tsx to display user data - needs to be changed when merged with future features

--- movie-checker/App.tsx
+++ movie-checker/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import UserProfilePage from './App/component/page/UserProfile';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <UserProfilePage/>
+    </PaperProvider>
   );
 }
 
